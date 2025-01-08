784936186ef46263b19ae6ee6607ec2a71830691
Mohammed Eid Abdelmeguid Ibrahim <mohammedeidabdelmeguid@gmail.com>
2025-01-08 18:28:17 +0200

    Remove unused import for easy_localization in home.dart

--- tourist_guide/lib/screens/home/home.dart
+++ tourist_guide/lib/screens/home/home.dart
@@ -1,4 +1,3 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:tourist_guide/screens/home/widgets/app_bar.dart';
 import 'package:tourist_guide/screens/home/widgets/popular_places.dart';
